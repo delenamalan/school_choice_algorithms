2137e06d7c435d0c46b026011ad0e2030df8e92c
Delena Malan <delenamalan@gmail.com>
2018-05-20 01:01:10 +0200

    Add cleaned up Boston Mechanism

--- python_implementations/boston_mechanism.py
+++ python_implementations/boston_mechanism.py
@@ -2,78 +2,79 @@
 Implementation of the Boston Mechanism algorithm from:
 https://www.bc.edu/content/dam/files/schools/cas_sites/economics/pdf/workingpapers/wp729.pdf
 """
-# I = ('Pete Michelin', 'Mpilo Kwathwane', 'Prince Sibanda') # Set of students
-I = (1,2,3,4,5)
-# C = ('Huguenot High School', 'Virginia Tech') # Set of schools
-C = ('a', 'b', 'c', 'd', 'e')
-# q = (2, 1) # Quota for each school
-q = [1 for x in xrange(5)]
+I = (1, 2, 3, 4, 5)# Set of students
+C = ('a', 'b', 'c', 'd', 'e') # Set of schools
+q = [1 for x in xrange(5)] # Quota for each school
 
 # Strict preference relation of each student
-# First index is student, second is school's ranking
-# P = ((1, 2), (2, 1), (1, 2))
-# P = ((2, 100, 100, 1, 3), (6, 1, 4, 2, 3), (100, 100, 100, 1, 100), (1, 100,
-#     100, 100, 100), (100, 1, 100, 100, 100))
+# List of school choices for each student in order of preference.
+# -1 is a "null school": it means that the student would prefer to rather go to
+# a school outside of the given choices.
 P = ((3, 0, 4), (1, 3, 4, 2, -1, 0), (3,), (0,), (1,))
 
 # Priority order
-# First index is student, second is the school and the priority of the student
-# at that school
-# Note: I'm assuming each student get a unique priority order at each school
-# PO = ((3, 1), (0, 2), (5, 0))
-# PO = ((5, 5, 0, 3, 5), (3, 3, 0, 5, 4), (0, 0, 0, 4, 0), (2, 2, 4, 0, 0), (4, 4,
-#    5, 0, 0))
+# List of student priorities for each school in order of preference.
+# -1 is the vacant position. It means that the school would rather have a vacant
+# position than a student below the ranking of the vacant position.
 PO = ((0, 4, 1, 3), (0, 4, 1, 3), (4, 3, -1), (1, 2, 0), (0, 1))
 
-
-u = [0, 1, 0] # School match for each student
-
 def boston_mechanism(I, C, q, P, PO):
-    u = [-1 for i in P]
-    uc = [0 for i in I]
+    u = [-1 for i in P] # The school each student has been admitted to.
+    student_count = [0 for i in I] # The number of students each school has been admitted.
 
-    # While there are students left without assigned schools
-    unassigned = [i for i in xrange(len(P)) if u[i] == -1]
-    print("unassigned: {}".format(unassigned))
+    unadmitted = [i for i in xrange(len(P)) if u[i] == -1]
     choice = 0 # The current choice level
-    iteration = 0
-    while len(unassigned) > 0:
-        # For each school with empty seats left
+
+    # While there are students left without admitted schools
+    while len(unadmitted) > 0:
+        print("#" * 50)
+        print("Choice level: {}".format(choice+1))
+        # For each school...
         for sc in xrange(len(C)):
-            if uc[sc] == q[sc]:
+            # ...with empty spots left
+            if student_count[sc] == q[sc]:
                 continue
 
             # Consider the students who who rather choose a null school at this
             # choice level
-            null_students = [i for i in unassigned if len(P[i]) > choice and
+            null_students = [i for i in unadmitted if len(P[i]) > choice and
                     P[i][choice] == -1]
+            # Assign those school to a null school
             for ns in null_students:
-                u[ns] = 1000000
+                u[ns] = -100
+                print("Student {} gets admitted to a null school.".format(I[ns]))
+            # print("Students with null choice at choice level {}: {}".format(choice+1, [I[i] for i in null_students]))
 
-            # Consider only the students who listed the school as their current choice
-            students = [i for i in unassigned if len(P[i]) > choice and P[i][choice] == sc]
-            print("students with choice {} at school {}: {}".format(choice, C[sc], students))
+            # Consider only the students who listed the current school as their current choice
+            students = [i for i in unadmitted if len(P[i]) > choice and P[i][choice] == sc]
+            # print("Students with choice {} at school {}: {}".format(choice+1,
+            #     C[sc], [I[i] for i in students]))
 
             # Assign seats to the school following the priority order of the
-            # students at the school
-            # PO = ((0, 4, 1, 3), (0, 4, 1, 3), (4, 3, -1), (1, 2, 0), (0, 1))
-            # st_po = [(i, PO[i][sc]) for i in students]
-            # st_po.sort(key=lambda x: x[1], reverse=True)
-
-            # Assign seats until there are no seats left or no students left that
-            # put the school as their top priority
+            # students at the school until there are no spots left at the school
+            # or no students left that put the school as their top priority
             i = 0
-            while not uc[sc] == q[sc] and len(PO[sc]) > i:
+            while not student_count[sc] == q[sc] and len(PO[sc]) > i:
+                # Check if the student on the school's priority list are in the
+                # group of unadmitted students
                 if PO[sc][i] in students:
-                    uc[sc] += 1
+                    # Assign the student to the school
                     u[PO[sc][i]] = sc
+                    student_count[sc] += 1
+                    print("Student {} admitted to school {}.".format(I[PO[sc][i]], C[sc]))
                 i += 1
 
 
-        unassigned = [i for i in xrange(len(P)) if u[i] == -1]
+        # Get the new group of unadmitted students
+        unadmitted = [i for i in xrange(len(P)) if u[i] == -1]
         choice += 1
-        print(u)
-        iteration += 1
-        if iteration > 5:
-            break
+        for student, school in enumerate(u):
+            print("{}:  {}.".format(I[student], C[school] if
+                school > -1 else "Null school"))
     return u
+
+u = boston_mechanism(I, C, q, P, PO)
+print("*" * 50)
+for student, school in enumerate(u):
+    print("Student {} is admitted to school {}.".format(I[student], C[school] if
+        school > -1 else "a null school"))
