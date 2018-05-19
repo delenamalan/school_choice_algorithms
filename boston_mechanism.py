80ce86101ded46307a9acf0c4bcdfd1989da03e3
Delena Malan <delenamalan@gmail.com>
2018-05-20 00:09:52 +0200

    Add Boston Mechanism without null school

--- boston_mechanism.py
+++ boston_mechanism.py
@@ -18,32 +18,43 @@ PO = ((3, 1), (0, 2), (5, 0))
 
 u = [0, 1, 0] # School match for each student
 
-def boston_mechanism(I, c, q, P, PO):
+def boston_mechanism(I, C, q, P, PO):
     u = [-1 for i in P]
     uc = [0 for i in I]
 
     # While there are students left without assigned schools
-    u = [i for i in xrange(len(P)) if u[i] == -1]
-    while len(u) > 0:
+    unassigned = [i for i in xrange(len(P)) if u[i] == -1]
+    print("unassigned: {}".format(unassigned))
+    choice = 1 # The current choice level
+    iteration = 0
+    while len(unassigned) > 0:
         # For each school with empty seats left
-        for sc in len(C):
-            if uc[sc] == a[sc]:
+        for sc in xrange(len(C)):
+            if uc[sc] == q[sc]:
                 continue
+
             # Consider only the students who listed the school as their top choice
-            students = [i for i in xrange(len(P)) if P[i][sc] == 1]
+            students = [i for i in unassigned if P[i][sc] == choice]
+            print("students with choice {} at school {}: {}".format(choice, sc, students))
 
             # Assign seats to the school following the priority order of the
             # students at the school
             st_po = [(i, PO[i][sc]) for i in students]
-            st_po.sort(key lamdba x: x[1], reverse=True)
+            st_po.sort(key=lambda x: x[1], reverse=True)
 
             # Assign seats until there are no seats left or no students left that
             # put the school as their top priority
             i = 0
-            while not uc[sc] > q[sc] and len(st_po) > 0 and st_po[i][0] > 0:
+            while not uc[sc] == q[sc] and len(st_po) > 0 and st_po[i][0] > 0:
                 uc[sc] += 1
-                u[st_po[i][1]] = sc
+                u[st_po[i][0]] = sc
                 i += 1
 
 
-        u = [i for i in xrange(len(P)) if u[i] == -1]
+        unassigned = [i for i in xrange(len(P)) if u[i] == -1]
+        choice += 1
+        print(u)
+        iteration += 1
+        if iteration > 2:
+            break
+    return u
